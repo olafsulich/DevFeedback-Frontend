6aff1f1e1d90166c467e12084db31a060fe2ae4e
Olaf Sulich <olafsulich@gmail.com>
2020-08-25 12:26:40 +0200

    Add ErrorBoundary to _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/node';
 import type { AppProps } from 'next/app';
 import getConfig from 'next/config';
 import { RewriteFrames } from '@sentry/integrations';
+import ErrorBoundary from '../shared/components/errorBoundary/ErrorBoundary';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -26,6 +27,9 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
 
 type Props = AppProps & { err: any };
 
-const App = ({ Component, pageProps, err }: Props) => <Component {...pageProps} err={err} />;
-
+const App = ({ Component, pageProps, err }: Props) => (
+  <ErrorBoundary>
+    <Component {...pageProps} err={err} />;
+  </ErrorBoundary>
+);
 export default App;

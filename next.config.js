ebebe9196267a09c6e14d5d1eeb8c0673d9d768e
Olaf Sulich <olafsulich@gmail.com>
2020-08-25 11:45:22 +0200

    Add Sentry configuration in next config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,69 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const withImages = require('next-images');
+const withSourceMaps = require('@zeit/next-source-maps')();
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
-  reactStrictMode: true,
-});
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+  VERCEL_GITHUB_COMMIT_SHA,
+} = process.env;
+
+process.env.SENTRY_DSN = SENTRY_DSN;
+
+const basePath = '';
+
+const config = withSourceMaps(
+  withImages({
+    serverRuntimeConfig: {
+      rootDir: __dirname,
+    },
+    webpack: (config, options) => {
+      if (!options.isServer) {
+        config.resolve.alias['@sentry/node'] = '@sentry/browser';
+      }
+
+      if (
+        SENTRY_DSN &&
+        SENTRY_ORG &&
+        SENTRY_PROJECT &&
+        SENTRY_AUTH_TOKEN &&
+        VERCEL_GITHUB_COMMIT_SHA &&
+        NODE_ENV === 'production'
+      ) {
+        config.plugins.push(
+          new SentryWebpackPlugin({
+            include: '.next',
+            ignore: ['node_modules'],
+            stripPrefix: ['webpack://_N_E/'],
+            urlPrefix: `~${basePath}/_next`,
+            release: VERCEL_GITHUB_COMMIT_SHA,
+          }),
+        );
+      }
+      return config;
+    },
+    basePath,
+  }),
+);
+
+config.pwa = {
+  dest: 'public',
+  runtimeCaching,
+};
+
+config.sassOptions = {
+  includePaths: [path.join(__dirname, 'styles')],
+};
+
+config.reactStrictMode = true;
+
+config.experimental = {
+  modern: true,
+};
+
+module.exports = config;

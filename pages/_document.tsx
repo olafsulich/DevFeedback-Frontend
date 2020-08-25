727272945a3bc7e9df2d02f6aa0afa9b59f8257d
Olaf Sulich <olafsulich@gmail.com>
2020-08-25 20:08:12 +0200

    Add custom Document page

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Main, Head, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <html
+        lang="pl-PL"
+        prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#"
+        itemScope
+        itemType="http://schema.org/WebPage"
+      >
+        <Head>
+          <base href="/" />
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=yes, initial-scale=1.0, viewport-fit=cover"
+          />
+          <meta property="og:type" content="website" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default MyDocument;

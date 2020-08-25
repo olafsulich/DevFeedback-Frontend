d050a22136fb42f37cba0e32dad3d3f6502ec7c3
Olaf Sulich <olafsulich@gmail.com>
2020-08-25 12:46:11 +0200

    Add PropsWithAs utility type

--- shared/types/types.ts
+++ shared/types/types.ts
@@ -3,3 +3,10 @@ import { ReactNode } from 'react';
 export type Nil<T> = T | null | undefined;
 
 export type WithChildren<T = {}> = T & { children: ReactNode };
+
+type As<Props = any> = React.ElementType<Props>;
+
+export type PropsWithAs<Props = {}, Type extends As = As> = Props &
+  Omit<React.ComponentProps<Type>, 'as' | keyof Props> & {
+    as?: Type;
+  };

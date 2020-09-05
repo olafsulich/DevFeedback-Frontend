85e9bcaba1d141d2d50b75ab3ee18a8b682f3ae4
Olaf Sulich <olafsulich@gmail.com>
2020-09-05 13:17:39 +0200

    Add basic styles for PrivacyPolicyModal

--- components/modals/privacyPolicyModal/PrivacyPolicyModal.tsx
+++ components/modals/privacyPolicyModal/PrivacyPolicyModal.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import styles from './privacyPolicyModal.module.scss';
+import Link from 'next/link';
+import CloseIcon from '../../../public/icons/close.svg';
 
 const PrivacyPolicyModal = () => {
   return (
-    <div className={styles.wrapper}>
-      <div>
-        <button>close</button>
-        <p>Korzystając z tej strony akceptujesz naszą politykę prywatności</p>
+    <dialog open className={styles.wrapper}>
+      <div className={styles.card}>
+        <button className={styles.closeButton}>
+          <CloseIcon />
+        </button>
+        <div className={styles.textContentBox}>
+          <p className={styles.textContentBoxText}>
+            Korzystając z tej strony akceptujesz naszą
+            <Link href="/">
+              <a className={styles.textContentBoxLink}>politykę prywatności</a>
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </dialog>
   );
 };
 

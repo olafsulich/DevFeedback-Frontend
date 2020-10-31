3aa3efc8255e3bafc28cd7761e1d770b8636277b
Olaf Sulich <olafsulich@gmail.com>
2020-10-31 02:39:45 +0100

    Add memoization to handleToggleMenu function

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,5 @@
 import styles from './header.module.scss';
-import { useState } from 'react';
-import cn from 'classnames';
-import Search from 'components/header/search/Search';
+import { useState, useCallback } from 'react';
 import Submenu from 'components/header/submenu/Submenu';
 import Logo from 'components/header/logo/Logo';
 import Navigation from 'components/header/navigation/Navigation';
@@ -10,9 +8,9 @@ import HamburgerButton from './hamburgerButton/HamburgerButton';
 const Header = () => {
   const [isVisible, setVisibility] = useState(false);
 
-  const handleToggleMenu = () => {
+  const handleToggleMenu = useCallback(() => {
     setVisibility((prevState) => !prevState);
-  };
+  }, []);
 
   return (
     <header className={styles.header}>

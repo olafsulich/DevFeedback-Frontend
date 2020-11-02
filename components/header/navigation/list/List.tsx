98b0f286033ac650eb677844dccc9bccdf889c68
Olaf Sulich <olafsulich@gmail.com>
2020-11-02 05:13:57 +0100

    Replace Links with AcitveLink component in List

--- components/header/navigation/list/List.tsx
+++ components/header/navigation/list/List.tsx
@@ -4,34 +4,35 @@ import styles from './list.module.scss';
 import CommentIcon from '../../../../public/icons/comment.svg';
 import ProjectIcon from '../../../../public/icons/project.svg';
 import HomeIcon from '../../../../public/icons/home.svg';
+import ActiveLink from '../activeLink/ActiveLink';
 
 const List = () => {
   return (
     <div className={styles.wrapper}>
       <ul className={styles.list} id="navigationList" aria-label="Dodatkowe informacje">
         <li className={styles.listItem}>
-          <Link href="/">
+          <ActiveLink href="/" activeClassName={styles.activeLink}>
             <a className={styles.link}>
               <HomeIcon aria-hidden="true" className={styles.icon} />
               Strona główna
             </a>
-          </Link>
+          </ActiveLink>
         </li>
         <li className={styles.listItem}>
-          <Link href="/">
+          <ActiveLink href="/projekty" activeClassName={styles.activeLink}>
             <a className={styles.link}>
               <ProjectIcon aria-hidden="true" className={styles.icon} />
               Moje projekty
             </a>
-          </Link>
+          </ActiveLink>
         </li>
         <li className={styles.listItem}>
-          <Link href="/">
+          <ActiveLink href="/feedback" activeClassName={styles.activeLink}>
             <a className={styles.link}>
               <CommentIcon aria-hidden="true" className={styles.icon} />
               Feedback
             </a>
-          </Link>
+          </ActiveLink>
         </li>
       </ul>
     </div>
